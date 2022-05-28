11c9269b6167968c8ddc75e48e5657fba64a9e19
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2022-05-28 22:05:34 +0700

    First Commit

---
+++ app/src/main/java/com/example/jualbukuid/ui/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jualbukuid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jualbukuid.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
