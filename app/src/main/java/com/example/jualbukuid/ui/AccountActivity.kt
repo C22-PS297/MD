6227244a85d33ce2c39a7562bdfc4c42b207f6e3
radhiathallah <radhiathallah298@gmail.com>
2022-06-05 22:18:12 +0700

    Unused Change AccountActitvity

--- app/src/main/java/com/example/jualbukuid/ui/AccountActivity.kt
+++ app/src/main/java/com/example/jualbukuid/ui/AccountActivity.kt
@@ -2,11 +2,16 @@ package com.example.jualbukuid.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.example.jualbukuid.R
 
 class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
     }
 }
